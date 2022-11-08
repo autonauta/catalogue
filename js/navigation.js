573c3111fc0981f632b447a6a3114974e6761a4c
César Alanís <cesar@highdatamx.com>
2022-11-08 17:23:21 -0600

    aded paquetes popup

--- js/navigation.js
+++ js/navigation.js
@@ -102,7 +102,39 @@ const navigateTo = (page) => {
       }, 300);
 
       break;
+      case "paquetes":
+        $(".section-alpha").css({ opacity: "0" });
+      setTimeout(() => {
+        $(".button-close").css({ opacity: "0" });
+        setTimeout(() => {
+          $(".section-alpha__top-line").css({ "background-size": "0% 1px" });
+          setTimeout(() => {
+            $("#popup-iluminacion").css({ visibility: "hidden", opacity: "0" });
+            $("#popup-seguridad").css({ visibility: "hidden", opacity: "0" });
+            $("#popup-vigilancia").css({ visibility: "hidden", opacity: "0" });
+            $("#popup-conectividad").css({ visibility: "hidden", opacity: "0" });
+            $("#popup-videoportero").css({ visibility: "hidden", opacity: "0" });
+            $("#popup-musica").css({ visibility: "hidden", opacity: "0" });
+            setTimeout(() => {
+              openPaquetes();
+            }, 400);
+          }, 600);
+        }, 400);
+      }, 300);
     default:
       break;
   }
 };
+
+function openPaquetes() {
+  $("#popup-paquetes").css({ visibility: "visible", opacity: "1" });
+  setTimeout(() => {
+    $(".section-alpha__top-line").css({ "background-size": "100% 1px" });
+    setTimeout(() => {
+      $(".button-close").css({ opacity: "1" });
+      setTimeout(() => {
+        $(".section-alpha").css({ opacity: "1" });
+      }, 300);
+    }, 400);
+  }, 300);
+}
