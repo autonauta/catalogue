766d93d976c73c7904df0b13bffc611d67fbd800
César Alanís <cesar@highdatamx.com>
2022-11-24 20:06:40 -0600

    up

---
+++ js/paquetes.js
@@ -0,0 +1,178 @@
+export const paquetes = {
+  iluminacion: {
+    basico: [
+      {
+        sysId: "171282",
+        quantity: 1,
+      },
+      {
+        sysId: "171284",
+        quantity: 1,
+      },
+      {
+        sysId: "192924",
+        quantity: 1,
+      },
+    ],
+    medio: [
+      {
+        sysId: "171282",
+        quantity: 2,
+      },
+      {
+        sysId: "171284",
+        quantity: 3,
+      },
+      {
+        sysId: "192924",
+        quantity: 5,
+      },
+    ],
+    pro: [
+      {
+        sysId: "171282",
+        quantity: 4,
+      },
+      {
+        sysId: "171284",
+        quantity: 6,
+      },
+      {
+        sysId: "192924",
+        quantity: 7,
+      },
+    ],
+  },
+  seguridad: {
+    basico: [
+      {
+        sysId: "191682",
+        quantity: 1,
+      },
+      {
+        sysId: "189894",
+        quantity: 1,
+      },
+      {
+        sysId: "189881",
+        quantity: 1,
+      },
+      {
+        sysId: "189858",
+        quantity: 1,
+      },
+      {
+        sysId: "189857",
+        quantity: 2,
+      },
+    ],
+    medio: [
+      {
+        sysId: "191682",
+        quantity: 1,
+      },
+      {
+        sysId: "189894",
+        quantity: 1,
+      },
+      {
+        sysId: "189881",
+        quantity: 1,
+      },
+      {
+        sysId: "189858",
+        quantity: 2,
+      },
+      {
+        sysId: "189857",
+        quantity: 2,
+      },
+      {
+        sysId: "191817",
+        quantity: 1,
+      },
+      {
+        sysId: "189889",
+        quantity: 1,
+      },
+    ],
+    pro: [
+      {
+        sysId: "191682",
+        quantity: 1,
+      },
+      {
+        sysId: "189894",
+        quantity: 2,
+      },
+      {
+        sysId: "189881",
+        quantity: 2,
+      },
+      {
+        sysId: "189858",
+        quantity: 3,
+      },
+      {
+        sysId: "189857",
+        quantity: 5,
+      },
+      {
+        sysId: "191817",
+        quantity: 1,
+      },
+      {
+        sysId: "189889",
+        quantity: 2,
+      },
+    ],
+  },
+  vigilancia: {
+    basico: [
+      {
+        sysId: "194834",
+        quantity: 1,
+      },
+      {
+        sysId: "208152",
+        quantity: 1,
+      },
+      {
+        sysId: "204207",
+        quantity: 1,
+      },
+      {
+        sysId: "174448",
+        quantity: 1,
+      }
+    ],
+    medio: [
+      {
+        sysId: "171282",
+        quantity: 2,
+      },
+      {
+        sysId: "171284",
+        quantity: 3,
+      },
+      {
+        sysId: "192924",
+        quantity: 5,
+      },
+    ],
+    pro: [
+      {
+        sysId: "171282",
+        quantity: 4,
+      },
+      {
+        sysId: "171284",
+        quantity: 6,
+      },
+      {
+        sysId: "192924",
+        quantity: 7,
+      },
+    ],
+  },
+};
