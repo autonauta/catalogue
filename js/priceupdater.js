d58b85710807d1db0fec3b7db6886bf637b37cf7
César Alanís <cesar@highdatamx.com>
2023-03-02 11:58:45 -0600

    Update priceupdater.js

--- js/priceupdater.js
+++ js/priceupdater.js
@@ -352,7 +352,7 @@ const version = ["pro", "mid", "basic"];
 // FUNCTIONS
 async function getProducts() {
   try {
-    const response = await fetch("https://arq.highdatamx.com/api/v1/products");
+    const response = await fetch("https://diy.highdatamx.com/api/v1/products");
     const res = await response.json();
     if (!res) console.log("No products received");
     else {
